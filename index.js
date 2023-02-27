b61763290b429c38ce927e153a733d8d17571537
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-27 13:50:43 -0500

    rendered emotions on a screen

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { catsData } from '/data.js'
 const emotionsArray = []
+const emotionRadios = document.querySelector('#emotion-radios')
+
+
 function getEmotionsArray(cats){
     for(let cat of cats){
         for(let emotions of cat.emotionTags){
@@ -8,5 +11,14 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
-getEmotionsArray(catsData)
 
+
+function renderEmotionsRadios(cats){
+    const emotions = getEmotionsArray(cats)
+    let emotionString = ""
+    for (let emotion of emotions){
+        emotionString += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = emotionString
+}
+renderEmotionsRadios(catsData)
