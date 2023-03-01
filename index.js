49838adc53c37b9adb4883c0035c00ca1393626a
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-01 11:30:56 -0500

    refactored code for better readability

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const memeModal = document.querySelector('#meme-modal')
 const memeModalCloseBtn = document.querySelector('#meme-modal-close-btn')
 
 emotionRadios.addEventListener("change", changeColorOnClickedElem)
-getImageBtn.addEventListener("click", renderCat)
 memeModalCloseBtn.addEventListener("click", ()=> {
     memeModal.style.display = "none"
 })
+getImageBtn.addEventListener("click", renderCat)
 
 function changeColorOnClickedElem(e){
     const removeClass = document.querySelectorAll('.radio')
@@ -21,19 +21,18 @@ function changeColorOnClickedElem(e){
     const clickedElem = document.getElementById(e.target.id).parentElement
     clickedElem.classList.add("highlight")
 }
-function getMatchingCatsArray(){
-    const isGif = gifsOnlyOption.checked
-    const selectedEmotion = document.querySelector("input[type='radio']:checked").value
 
-    const matchingCatsArray = catsData.filter(emotion => {
-        if(isGif){
-            return  emotion.isGif && emotion.emotionTags.includes(selectedEmotion)
-        } else {
-            return emotion.emotionTags.includes(selectedEmotion)
-        }
-    })
-    return matchingCatsArray
+function renderCat(){
+    const catObject = getSingleCatObject() 
+    memeModalInner.innerHTML = `
+        <img  
+            class="cat-img"
+            src="/images/${catObject.image}"
+            alt="/images/${catObject.alt}">
+        `
+    memeModal.style.display= "flex"
 }
+
 function getSingleCatObject(){
     const catsArray = getMatchingCatsArray()
     if(catsArray.length === 1){
@@ -43,14 +42,22 @@ function getSingleCatObject(){
         return catsArray[randomNumber]
     }
 }
-function renderCat(){
-    const catObject = getSingleCatObject() 
-    memeModalInner.innerHTML = `<img class="cat-img"
-                                    src="/images/${catObject.image}"
-                                    alt="/images/${catObject.alt}">`
-    memeModal.style.display= "flex"
-    console.log(catObject.image)
+
+function getMatchingCatsArray(){
+    const isGif = gifsOnlyOption.checked
+    const selectedEmotion = document.querySelector("input[type='radio']:checked").value
+
+    const matchingCatsArray = catsData.filter(emotion => {
+        if(isGif){
+            return  emotion.isGif && emotion.emotionTags.includes(selectedEmotion)
+        } 
+        else {
+            return emotion.emotionTags.includes(selectedEmotion)
+        }
+    })
+    return matchingCatsArray
 }
+
 function getEmotionsArray(cats){
     for(let cat of cats){
         for(let emotions of cat.emotionTags){
@@ -61,6 +68,7 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
+
 function renderEmotionsRadios(cats){
     const emotions = getEmotionsArray(cats)
     let radioItems = ""
